f58f7b010a1cc8da90f5179bfe107a532acae796
LiMeng <baloo@foxmail.com>
2019-03-20 10:43:51 +0800

    v2

--- server/tsa_main.py
+++ server/tsa_main.py
@@ -22,21 +22,23 @@ def hello_world(province, city, region):
 
 @app.route('/tsa/<province>&<city>&<region>')
 def get_tsa(province, city, region):
-    try:
-        province = str(province).decode('string-escape')
-        city = str(city).decode('string-escape')
-        region = str(region).decode('string-escape')
-        print('province:{0}, city:{1}, region:{2}'.format(province, city, region))
-    except:
-        try:
-            province = urllib.parse.unquote(province)
-            city = urllib.parse.unquote(city)
-            region = urllib.parse.unquote(region)
-            print('province:{0}, city:{1}, region:{2}'.format(province, city, region))
-        except:
-            msg = Message(1, 'error')
-            return json.dumps(msg.__dict__, ensure_ascii=False)
-
+    # try:
+    #     province = str(province).decode('string-escape')
+    #     city = str(city).decode('string-escape')
+    #     region = str(region).decode('string-escape')
+    #     print('province:{0}, city:{1}, region:{2}'.format(province, city, region))
+    # except:
+    #     try:
+    #         province = urllib.parse.unquote(province)
+    #         city = urllib.parse.unquote(city)
+    #         region = urllib.parse.unquote(region)
+    #         print('province:{0}, city:{1}, region:{2}'.format(province, city, region))
+    #     except:
+    #         msg = Message(1, 'error')
+    #         return json.dumps(msg.__dict__, ensure_ascii=False)
+    province = urllib.parse.unquote(province)
+    city = urllib.parse.unquote(city)
+    region = urllib.parse.unquote(region)
     try:
         f = open(os.getcwd() + '/data/{0}{1}{2}.json'.format(province,city,region), 'r', encoding='utf-8')
         t = json.load(f)
